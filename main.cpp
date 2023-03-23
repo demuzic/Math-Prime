a8a7440a32aa6437b54f8b0ca9b60cbf63dc8733
Murilo Castro <57036399+demuzic@users.noreply.github.com>
2023-03-23 18:45:41 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@
 #include "PRIME.h"
 #include <chrono>
 #include <ctime>
-
-
-
+#include <math.h>
+#include <iomanip>
+#include "PI.h"
 using namespace std;
 
-#define commC 9
+const string VERSION = "0.75";
+
+#define commC 10
 
 #define mainColor 14
 #define hColor 192
@@ -24,23 +26,26 @@ using namespace std;
 
 string ccommand = "";
 string inp = "";
-int element_ = 0;
-int element = 0;
-string comm[commC] = {"r@","pr@","pc@","pv@","help","sys","pf@","ct@"};
+
+int element[3] = {0,0,0};
+string comm[commC] = {"r@","pr@","pc@","pv@","help","sys","pf@","ct@", "pi@"};
 int commN = 0;
 string screenTitle = "|";
 
 int systime = 0; //show elapsed timer
 
 
+
 int main() {
+	
 	auto start = std::chrono::system_clock::now();
 	auto end = std::chrono::system_clock::now();
 	std::chrono::duration<double> elapsed_seconds = end - start;
 
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 31);
-	cout << "-------------------------MATH PRIME - v0.7-------------------------" << endl;
-
+	cout << "-------------------------MATH PRIME | " <<  VERSION << "-------------------------" << endl;
+	cout << setprecision(15);
+	
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), mainColor);
 	while (true)
 	{
@@ -71,16 +76,16 @@ int main() {
 			commN = 0;
 			break;
 		
-		case 2:
+		case 2: //pr@
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), pColor);
 			cout << "> ";
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), iColor);
-			cin >> element;
+			cin >> element[0];
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), oColor);
 
 			if (systime) start = std::chrono::system_clock::now();
 
-			cout << prim(element) << endl;
+			cout << prim(element[0]) << endl;
 
 			if (systime) end = std::chrono::system_clock::now();
 			elapsed_seconds = end - start;
@@ -92,16 +97,16 @@ int main() {
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), mainColor);
 			break;
 		
-		case 3:
+		case 3: //pc@
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), pColor);
 			cout << "> ";
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), iColor);
-			cin >> element;
+			cin >> element[0];
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), oColor);
 
 			start = std::chrono::system_clock::now();
 
-			cout << primC(element) << endl;
+			cout << primC(element[0]) << endl;
 
 			end = std::chrono::system_clock::now();
 			elapsed_seconds = end - start;
@@ -111,16 +116,16 @@ int main() {
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), mainColor);
 			break;
 
-		case 4:
+		case 4: //pv@
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), pColor);
 			cout << "> ";
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), iColor);
-			cin >> element;
+			cin >> element[0];
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), oColor);
 			
 			if (systime) start = std::chrono::system_clock::now();
 
-			cout << primV(element) << endl;
+			cout << primV(element[0]) << endl;
 
 			if (systime) end = std::chrono::system_clock::now();
 			elapsed_seconds = end - start;
@@ -131,16 +136,17 @@ int main() {
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), mainColor);
 			break;
 
-		case 5:
+		case 5: //help
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), hColor);
 			cout << "x = user input number \n r@ - return 0 \n pr@ - return 1 or 0 if a number is prime \n pc@ - return the number of prime numbers up to the x \n pv@ - return the x number prime\
 			\n sys - insert system command \n ct@ - Choose 0 or 1 to disable or enable each function's elapsed time display\
-			\n pf@ - returns the prime number x (the inverse of pv@) " << endl;
+			\n pf@ - returns the prime number x (the inverse of pv@) \n pi@ - returns the approximate value of pi, enter the number of decimal places, count precision (the higher, the longer it takes to calculate) and loading percentage (0 or 1)\
+			" << endl;
 			commN = 0;
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), mainColor);
 			break;
 
-		case 6:
+		case 6: //sys
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), pColor);
 			cout << "> ";
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), xColor);
@@ -151,21 +157,21 @@ int main() {
 			if (ccommand == "cls")
 			{
 				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 31);
-				cout << "-------------------------MATH PRIME - v0.7-------------------------" << endl;
+				cout << "-------------------------MATH PRIME | " << VERSION << "-------------------------" << endl;
 			}
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), mainColor);
 			break;
 
 		
-		case 7:
+		case 7: //pf@
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), pColor);
 			cout << "> ";
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), iColor);
-			cin >> element;
+			cin >> element[0];
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), oColor);
 			if (systime) start = std::chrono::system_clock::now();
 
-			cout << primF(element) << endl;
+			cout << primF(element[0]) << endl;
 
 			if (systime) end = std::chrono::system_clock::now();
 			elapsed_seconds = end - start;
@@ -176,19 +182,47 @@ int main() {
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), mainColor);
 			break;
 
-		case 8:
+		case 8: //ct@
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), pColor);
 			cout << "> ";
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), iColor);
-			cin >> element;
-			if (element == 1) systime = 1;
+			cin >> element[0];
+			if (element[0] == 1) systime = 1;
 			else systime = 0;
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), oColor);
-			if (element == 1) cout << "ELAPSED TIME ON" << endl;
-			if (element == 0) cout << "ELAPSED TIME OFF" << endl;
+			if (element[0] == 1) cout << "ELAPSED TIME ON" << endl;
+			if (element[0] == 0) cout << "ELAPSED TIME OFF" << endl;
 			commN = 0;
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), mainColor);
 			break;
+
+		case 9: //PI
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), pColor);
+			cout << "> ";
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), iColor);
+			cin >> element[0];
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), pColor);
+			cout << "> ";
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), iColor);
+			cin >> element[1];
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), pColor);
+			cout << "> ";
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), iColor);
+			cin >> element[2];
+
+			if (systime) start = std::chrono::system_clock::now();
+
+			cout << pi(element[0], element[1], element[2], xColor, oColor) << endl;
+
+			if (systime) end = std::chrono::system_clock::now();
+			elapsed_seconds = end - start;
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), xColor);
+			if (systime) cout << "elapsed: " << elapsed_seconds.count() << "s" << endl;
+
+			commN = 0;
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), mainColor);
+			break;
+
 		default:
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), eColor);
 			cout << "error - '" + inp + "' not found in command array" << endl;
